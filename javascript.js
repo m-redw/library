b68d30c8c1e5082a4c88320e2b1433d3e3847bf5
Edwin <264506916+m-redw@users.noreply.github.com>
2026-06-06 15:58:55 -0700

    Create functional display function
    Also adjust html and css to accommodate for js display function.
    Also removed test mockup.

--- javascript.js
+++ javascript.js
@@ -15,6 +15,50 @@ function addBookToLibrary(title, author, pages, read) {
     myLibrary.push(book);
 }
 
-for (let i = 0; i < 5; i++) {
-    addBookToLibrary('test', 'test', 1, false);
+function displayBooks() {
+    const libraryContainer = document.querySelector('.library');
+    for (const book of myLibrary) {
+        // book card container
+        const bookCard = document.createElement('div');
+        bookCard.classList.add('book-card');
+
+        // book card content
+        const title = document.createElement('h3');
+        const author = document.createElement('h4');
+        const pageCount = document.createElement('p');
+        const readStatus = document.createElement('p');
+        const removeButton = document.createElement('button');
+        const changeReadButton = document.createElement('button');
+
+        // edit book card content
+        title.textContent = book.title;
+        author.textContent = `By ${book.author}`;
+        pageCount.textContent = `${book.pages} pages`;
+        if (book.read) {
+            readStatus.textContent = 'Read';
+            readStatus.classList.add('book-read');
+        } else {
+            readStatus.textContent = 'Not Read';
+            readStatus.classList.add('book-not-read');
+        }
+        removeButton.textContent = 'Remove';
+        changeReadButton.textContent = 'Change Read Status';
+        
+        bookCard.appendChild(title);
+        bookCard.appendChild(author);
+        bookCard.appendChild(pageCount);
+        bookCard.appendChild(readStatus);
+        bookCard.appendChild(removeButton);
+        bookCard.appendChild(changeReadButton);
+        libraryContainer.appendChild(bookCard);
+    }
 }
+
+addBookToLibrary('test1', 'tester1', 1, false);
+addBookToLibrary('test2', 'tester2', 2, true);
+addBookToLibrary('test3', 'tester3', 3, false);
+addBookToLibrary('test4', 'tester4', 4, false);
+addBookToLibrary('test5', 'tester5', 5, true);
+addBookToLibrary('test6', 'tester6', 6, true);
+
+displayBooks();
